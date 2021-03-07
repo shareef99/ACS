2d1be42cb130ee538f0aaadd7239d1875deb5385
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2021-03-07 11:00:52 +0530

    numeric type conversion

---
+++ numericDataTypeConversion.py
@@ -0,0 +1,15 @@
+a=10 
+b="Hi Python" 
+c = 10.5 
+d=2+3j
+print(type(a)) 
+print(type(b)) 
+print(type(c))
+print(type(d)) 
+#convert from int to float:
+print(float(a))
+#convert from float to int:
+print(int(c))
+#convert from int to complex:
+print(complex(a))
+print(complex(a,c))
