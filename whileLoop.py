87474a1bc04c6fa985cbcd676a0fc19c319cc7a7
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2021-03-07 11:19:51 +0530

    Program to add first "n" natural numbers

---
+++ whileLoop.py
@@ -0,0 +1,9 @@
+n = int(input("Enter n: "))
+# initialize sum and counter
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1 # update counter
+# print the sum
+print("Sum of first",n,"natural numbers is", sum)
