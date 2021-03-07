902272c7f56af73893249c608208a8782ff25f29
Mohammed Nadeem Shareef <nadeemshareef934@gmail.com>
2021-03-07 10:54:37 +0530

    A program to find max of two number
    short programs for college

---
+++ maxOfTwo.py
@@ -0,0 +1,9 @@
+a = int(input("First number: ")) 
+b = int(input("Second number: "))
+
+if a>b:
+    print(a , " is greater than " , b)
+elif b>a:
+    print(b , " is greater than " , a)
+else:
+    print(a , " and " , b , " are equal")
